214aa2bc8ec82fd073e22437ea26d6f962393d6f
alok-kiran <alokkiran777@gmail.com>
2024-02-17 01:13:59 +0400

    added settings page

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -10,7 +10,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 top-0 md:px-4 w-full h-14 border-b shadow-sm bg-white flex items-center max-sm:pr-4">
         <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
