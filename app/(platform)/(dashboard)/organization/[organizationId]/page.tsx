3a458c378a75b39e920b56c060cfba2abf68aabc
alok-kiran <alokkiran777@gmail.com>
2024-06-01 22:25:59 +0400

    added proper style to boardlist

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Info } from './_components/info'
 import { Separator } from '@/components/ui/separator'
 import { BoardList } from './_components/board-list'
@@ -10,7 +10,9 @@ async function OrganizationPage() {
         <Info />
         <Separator className='my-4'/>
         <div className=' px-2 md:px-4'>
+          <Suspense fallback={<BoardList.Skeleton />}>
            <BoardList /> 
+          </Suspense>
         </div>
     </div>
   )
