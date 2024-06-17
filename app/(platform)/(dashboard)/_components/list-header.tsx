3bd09cd0bf7634d3ea741b8cb52035bcfcad5e17
alok-kiran <alokkiran777@gmail.com>
2024-06-17 22:46:17 +0400

    added ref to form

--- app/(platform)/(dashboard)/_components/list-header.tsx
+++ app/(platform)/(dashboard)/_components/list-header.tsx
@@ -69,7 +69,12 @@ const ListHeader = ({
         <div className=" pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
             {
                 isEditing ? (
-                   <form className=" flex-1 px-[2px" action={handleSubmit}>
+                   <form 
+                        className=" flex-1 px-[2px" 
+                        action={handleSubmit}
+                        ref={formRef}
+                        
+                        >
                         <input 
                             hidden
                             id="id"
